270e39ecd3ea8e66fea6e6c3ae4175cde33740ff
Ricardo Cabrera <ricky_92rc@hotmail.com>
2019-09-21 16:04:36 -0700

    created departments, and product

---
+++ Customer.js
@@ -0,0 +1,150 @@
+
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+ 
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "ucbsf2019",
+  database: "bamazon_db"
+});
+
+
+ 
+connection.connect(function(err) {
+  if (err) throw err;
+
+  Show_Products();
+
+});
+
+ 
+var Show_Products = function() {
+	var query = "Select item_id,product_name,department_name,price,stock from products lef join departments using (department_id)";
+	connection.query(query, function(err, res) {
+
+		if (err) throw err; 
+		console.table(res);
+  		Call_Product();
+	});
+};
+
+ 
+var Call_Product = function() {
+	inquirer.prompt([{
+		name: "productID",
+		type: "input",
+		message: "Please enter product ID for product you want.",
+		validate: function(value) {
+			if (isNaN(value) === false) {
+				return true;
+			}
+			return false;
+		}
+	}, {
+		name: "productUnits",
+		type: "input",
+		message: "How many units do you want?",
+		validate: function(value) {
+			if (isNaN(value) === false) {
+				return true;
+			}
+			return false
+		}
+	},]).then(function(answer) {
+
+ 
+		var query = "Select stock, price, product_sales, department_id FROM products WHERE ?";
+		connection.query(query, { item_id: answer.productID}, function(err, res) {
+			
+			if (err) throw err;
+
+			var available_stock = res[0].stock;
+			var price_per_unit = res[0].price;
+			var productSales = res[0].product_sales;
+			var productDepartment = res[0].department_id;
+
+		 
+			if (available_stock >= answer.productUnits) {
+				
+				completePurchase(available_stock, price_per_unit, productSales, productDepartment, answer.productID, answer.productUnits);
+			} else {
+
+				console.log("There isn't enough stock left!");
+
+				Call_Product();
+			}
+		});
+	});
+};
+
+
+ 
+var completePurchase = function(availableStock, price, productSales, productDepartment, selectedProductID, selectedProductUnits) {
+	
+ 
+	var updatedStockQuantity = availableStock - selectedProductUnits;
+
+	 
+	var totalPrice = price * selectedProductUnits;
+
+ 
+	var updatedProductSales = parseInt(productSales) + parseInt(totalPrice);
+	
+ 
+	var query = "UPDATE products SET ? WHERE ?";
+	connection.query(query, [{
+		stock: updatedStockQuantity,
+		product_sales: updatedProductSales
+	}, {
+		item_id: selectedProductID
+	}], function(err, res) {
+
+		if (err) throw err;
+	 
+		console.log("Your purchase is complete.");
+
+	 
+		console.log("You payment has been received in the amount of : " + totalPrice);
+
+	 
+		
+	});
+};
+
+ 
+
+var updateDepartmentRevenue = function(updatedProductSales, productDepartment) {
+
+	 
+	var query = "Select total_sales FROM departments WHERE ?";
+	connection.query(query, { department_id:productDepartment}, function(err, res) {
+
+		if (err) throw err;
+
+		var departmentSales = res[0].total_sales;
+
+		var updatedDepartmentSales = parseInt(departmentSales) + parseInt(updatedProductSales);
+
+		 
+		completeDepartmentSalesUpdate(updatedDepartmentSales, productDepartment);
+	});
+};
+
+
+
+var completeDepartmentSalesUpdate = function(updatedDepartmentSales, productDepartment) {
+
+	var query = "UPDATE departments SET ? WHERE ?";
+	connection.query(query, [{
+		total_sales: updatedDepartmentSales
+	}, {
+		department_name: productDepartment
+	}], function(err, res) {
+
+		if (err) throw err;
+		Show_Products();
+	});
+};
--- bamazon.sql
+++ bamazon.sql
@@ -3,68 +3,67 @@ DROP DATABASE bamazon_db;
 CREATE DATABASE bamazon_db;
 
 USE bamazon_db;
+CREATE TABLE departments (
+  department_id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(255) NOT NULL,
+  over_head_costs INT NOT NULL,
+  total_sales INT NOT NULL DEFAULT '0',
 
-CREATE TABLE product (
-    id INT AUTO_INCREMENT,
-    product_name VARCHAR(255) NOT NULL,
-    department_name VARCHAR(255) NOT NULL,
-    price VARCHAR(255) NOT NULL,
-    stock_quantity VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+  PRIMARY KEY (department_id)
 );
 
-/*CREATE TABLE  (
-    id INT AUTO_INCREMENT,
-    car_name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE products (
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(255) NOT NULL,
+  department_id INT NOT NULL,
+  price DECIMAL(7,2) NOT NULL,
+  stock INT NOT NULL DEFAULT '1' ,
+    FOREIGN KEY (department_id) REFERENCES departments(department_id),
+      PRIMARY KEY (item_id)
 );
-CREATE TABLE parts (
-    id INT AUTO_INCREMENT,
-    part_name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
-);
+ 
 
-CREATE TABLE person_cars (
-    car_id INT NOT NULL,
-    FOREIGN KEY (car_id) REFERENCES cars(id),
-    person_id INT NOT NULL,
-    FOREIGN KEY (person_id) REFERENCES person(id)
-);
+ALTER TABLE products ADD COLUMN product_sales INT NOT NULL DEFAULT '1';
 
-CREATE TABLE car_parts (
-    car_id INT NOT NULL,
-    FOREIGN KEY (car_id) REFERENCES cars(id),
-    part_id INT NOT NULL,
-    FOREIGN KEY (part_id) REFERENCES parts(id)
-);
 
-INSERT INTO person (name) VALUES 
-('steph curry'),  
-('lionel messi'), 
-('cristiano ronaldo');
 
-INSERT INTO cars (car_name) VALUES 
-('toyota highlander'), 
-('bugati ceron'),
-('alpha romero'),
-('chevy cruise');
+INSERT INTO bamazon_db.departments
+(department_id,
+department_name,
+over_head_costs,
+total_sales)
+VALUES
+(1,
+"Home Theater",
+5000 ,
+0),(2,
+"Computers",
+30000.00 ,
+0),(3,
+"Appliances",
+150000.00 ,
+0),(4,
+"Electronics",
+20000.00 ,
+0);
 
-INSERT INTO parts (part_name) VALUES 
-('wheel'),
-('break'),
-('spark plugs');
 
-INSERT INTO person_cars (car_id, person_id) VALUES 
-(1,1), 
-(4,2),
-(2,3),
-(3,1),
-(3,2);
-
-INSERT INTO car_parts (car_id, part_id) VALUES 
-(1,3), 
-(2,2),
-(2,1),
-(3,2),
-(3,3),
-(3,1); */
+INSERT INTO bamazon_db.products
+(item_id,
+product_name,
+department_id,
+price,
+stock)
+VALUES
+(1,"Samsung 70",1,1500,8),
+(2,"Sony 90",1,2000,5),
+(3,"Apple Macbook",2,1500,5),
+(4,"Dell 200x",2 ,500,10),
+(5,"Windows Surface",2,1300,7),
+(6,"Toshiba 300p",2,800,4),
+(7,"Oven",3,1000,3),
+(8,"Refrigerator",3,3000,4),
+(9,"Microwave",3,500,5),
+(10,"Beats Headphones",4,300,15),
+(11," Iphone Charger",4,30,19),
+(12,"Mophie charger",4,100,8);
